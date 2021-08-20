8cb8432f3f761bdadd3cc3e822ba63e189065066
Linh <d.linh1196@gmail.com>
2021-08-20 14:08:44 +0700

    Init

---
+++ app/src/main/java/com/example/a08202021_7_minute_workout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a08202021_7_minute_workout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
