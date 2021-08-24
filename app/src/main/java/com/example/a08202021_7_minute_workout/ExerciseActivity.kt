cac539a82a27fcd804c9587f3f1e0c59017610b1
Linh <d.linh1196@gmail.com>
2021-08-24 18:16:09 +0700

    Using Countdown Timer to display the seconds remaining

--- app/src/main/java/com/example/a08202021_7_minute_workout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a08202021_7_minute_workout/ExerciseActivity.kt
@@ -2,9 +2,14 @@ package com.example.a08202021_7_minute_workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +23,41 @@ class ExerciseActivity : AppCompatActivity() {
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    override fun onDestroy() {
+        if(restTimer != null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
+    //hard code 10s
+    private fun setRestProgressBar() {
+        progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                progressBar.progress = 10-restProgress
+                tvTimer.text = (10- restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Here now we start the exercise.", Toast.LENGTH_SHORT).show()
+
+            }
+        }.start()
+    }
+
+    private fun setupRestView() {
+        if(restTimer!= null) {
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
     }
 }
